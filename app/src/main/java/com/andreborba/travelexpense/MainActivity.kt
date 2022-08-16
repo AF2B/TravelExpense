9ed926e86d3457172d2ebc78f5abc23e41540a19
andre.borba <dev_andre.filipe@outlook.com>
2022-08-16 13:09:13 -0300

    identação ajustada

--- app/src/main/java/com/andreborba/travelexpense/MainActivity.kt
+++ app/src/main/java/com/andreborba/travelexpense/MainActivity.kt
@@ -7,16 +7,16 @@ import android.widget.Toast
 import com.andreborba.travelexpense.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-		private lateinit var binding: ActivityMainBinding
+	private lateinit var binding: ActivityMainBinding
 
-		override fun onCreate(savedInstanceState: Bundle?) {
-				super.onCreate(savedInstanceState)
-				setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-				binding = ActivityMainBinding.inflate(layoutInflater)
-				setContentView(binding.root)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-				binding.buttonCalculate.setOnClickListener(this)
+		binding.buttonCalculate.setOnClickListener(this)
 //        binding.buttonCalculate.setOnClickListener(object : View.OnClickListener {
 //            override fun onClick(v: View?) {
 //                TODO("Not yet implemented")
@@ -24,34 +24,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //        })
 		}
 
-		override fun onClick(view: View) {
-				if (view.id == R.id.button_calculate) {
-						calculate()
-				}
+	override fun onClick(view: View) {
+		if (view.id == R.id.button_calculate) {
+			calculate()
 		}
+	}
 
-		private fun calculate() {
-				if (isValid()) {
-						val distance = binding.editDistance.text.toString().toFloat()
-						val price = binding.editPrice.text.toString().toFloat()
-						val autonomy = binding.editAutonomy.text.toString().toFloat()
+	private fun calculate() {
+		if (isValid()) {
+			val distance = binding.editDistance.text.toString().toFloat()
+			val price = binding.editPrice.text.toString().toFloat()
+			val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-						val result = (distance / autonomy) * price
-						val resultFormatted = "%.2f".format(result)
+			val result = (distance / autonomy) * price
+			val resultFormatted = "%.2f".format(result)
 
-						binding.textTotalPrice.text = resultFormatted
+			binding.textTotalPrice.text = resultFormatted
 						//Toast.makeText(this, resultFormatted, Toast.LENGTH_SHORT).show()
-				}
 		}
+	}
 
-		private fun isValid(): Boolean {
-				if (binding.editDistance.text.toString()
-								.isEmpty() || binding.editPrice.text.toString()
-								.isEmpty() || binding.editAutonomy.text.toString()
-								.isEmpty() && binding.editAutonomy.text.toString().toFloat() != 0.0f
+	private fun isValid(): Boolean {
+		if (binding.editDistance.text.toString()
+				.isEmpty() || binding.editPrice.text.toString()
+				.isEmpty() || binding.editAutonomy.text.toString()
+				.isEmpty() && binding.editAutonomy.text.toString().toFloat() != 0.0f
 				) {
 						Toast.makeText(this, R.string.validate_fill_fields, Toast.LENGTH_LONG).show()
 				}
-				return true
+			return true
 		}
 }
